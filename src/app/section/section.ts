e9b249fc48d1cdd107a7edf240dad63bbfaba1ee
palllaura <palllaura.tln@gmail.com>
2025-07-09 16:52:47 +0300

    Fixed horizontal scroll buttons visibility

--- src/app/section/section.ts
+++ src/app/section/section.ts
@@ -9,21 +9,36 @@ import { CommonModule } from '@angular/common';
   templateUrl: './section.html',
   styleUrls: ['./section.css']
 })
+
 export class Section {
   @Input() title!: string;
-  @Input() items: any[] = [];@ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
+  @Input() items: any[] = [];
+  @ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
+
+  showLeftButton = false;
+  showRightButton = false;
+
+  ngAfterViewInit() {
+    this.updateButtonVisibility();
+  }
 
   scrollLeft(): void {
-    this.scrollContainer.nativeElement.scrollBy({
-      left: -1000,
-      behavior: 'smooth'
-    });
+    this.scrollContainer.nativeElement.scrollBy({ left: -1000, behavior: 'smooth' });
+    setTimeout(() => this.updateButtonVisibility(), 300);
   }
 
   scrollRight(): void {
-    this.scrollContainer.nativeElement.scrollBy({
-      left: 1000,
-      behavior: 'smooth'
-    });
+    this.scrollContainer.nativeElement.scrollBy({ left: 1000, behavior: 'smooth' });
+    setTimeout(() => this.updateButtonVisibility(), 300);
+  }
+
+  updateButtonVisibility(): void {
+    const el = this.scrollContainer.nativeElement;
+    this.showLeftButton = el.scrollLeft > 0;
+    this.showRightButton = el.scrollLeft + el.clientWidth < el.scrollWidth;
+  }
+
+  onScroll(): void {
+    this.updateButtonVisibility();
   }
 }
